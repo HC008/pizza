b7097f27145a524b274162334796c0defd70b5f2
HC008 <h.wk.cheng@gmail.com>
2014-06-03 20:42:40 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pizza::Application.config.secret_key_base = 'd5938b8835c0d9fee13ef9f27f4d9ffde1ef58291f5a6b05f9e52d1a98afb7d94ca1fafb5f2e97c01a26b29cd7c1fbf76fd0555e619f36eb0cc81ef07410c399'
